8e611a3cdc16978a1065b28a610fe49ea6ecca81
Phil Dubem <phildubem@gmail.com>
2020-05-05 18:32:00 +0100

    Final Commit

--- app/src/main/java/com/thebereau/startportfolioapp/LoginActivity.kt
+++ app/src/main/java/com/thebereau/startportfolioapp/LoginActivity.kt
@@ -5,8 +5,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.method.HideReturnsTransformationMethod
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.login_screen.*
+import kotlinx.android.synthetic.main.signup_screen.*
 
 class LoginActivity : AppCompatActivity () {
 
@@ -19,18 +21,21 @@ class LoginActivity : AppCompatActivity () {
 
         prefProv = PrefProv(applicationContext)
 
-        btnLog.setOnClickListener {
-            etL1.setText(prefProv.getString(Constants.EMAIL))
-            etL2.setText(prefProv.getString(Constants.PASSWORD))
-
-            val mainIntent = Intent (this, MainActivity::class.java)
-            startActivity(mainIntent)
-        }
 
         val pw = findViewById<TextView>(R.id.etL2)
         pw.setOnClickListener {
             pw.transformationMethod = HideReturnsTransformationMethod.getInstance()
         }
+
+        btnLog.setOnClickListener {
+            etL1.setText(prefProv.getString(Constants.EMAIL))
+            etL2.setText(prefProv.getString(Constants.PASSWORD))
+
+
+            val mainIntent = Intent (this, MainActivity::class.java)
+            startActivity(mainIntent)
+        }
+
 
     }
 
--- app/src/main/java/com/thebereau/startportfolioapp/SignUp.kt
+++ app/src/main/java/com/thebereau/startportfolioapp/SignUp.kt
@@ -4,8 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.method.HideReturnsTransformationMethod
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.signup_screen.*
+
+
 
 
 class SignUp : AppCompatActivity() {
@@ -18,24 +21,34 @@ class SignUp : AppCompatActivity() {
 
         prefProv = PrefProv(applicationContext)
 
-        btnSin.setOnClickListener {
-            prefProv.putString(Constants.NAME, etS1.text.toString())
-            prefProv.putString(Constants.EMAIL, etS2.text.toString())
-            prefProv.putInt(Constants.PHONE, etS3.text.toString().toInt())
-            prefProv.putString(Constants.PASSWORD, etS4.text.toString())
-
-
-            val loginIntent = Intent (this, LoginActivity::class.java)
-            startActivity(loginIntent)
-
-        }
 
         val pw = findViewById<TextView>(R.id.etS4)
         pw.setOnClickListener {
             pw.transformationMethod = HideReturnsTransformationMethod.getInstance()
         }
 
+        btnSin.setOnClickListener {
+            prefProv.putString(Constants.NAME, etS1.text.toString())
+            prefProv.putString(Constants.EMAIL, etS2.text.toString())
+            prefProv.putString(Constants.PHONE, etS3.text.toString())
+            prefProv.putString(Constants.PASSWORD, etS4.text.toString())
+
+
+            val loginIntent = Intent (this, LoginActivity::class.java)
+            if (etS1.text.isEmpty() || etS2.text.isEmpty()) {
+                Toast.makeText(applicationContext, "All Fields Required", Toast.LENGTH_LONG).show()
+            } else if(etS3.text.isEmpty() || etS4.text.isEmpty()) {
+                Toast.makeText(applicationContext, "All Fields Required", Toast.LENGTH_LONG).show()
+            }else {
+                startActivity(loginIntent)
+            }
+
+        }
+
+
     }
+
+
 
 
 }
